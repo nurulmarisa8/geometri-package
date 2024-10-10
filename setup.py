dcb2dcd54f62d13d612e20defc971a67948b900b
nurulmarisa <nurulmarisaclarawaldi8@gmail.com>
2024-10-10 12:55:52 +0800

    package-geometri

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="package_geometri",
+    name="geometri-package",
     version="0.1",
     packages=find_packages(),
-    description="A package for calculating various properties of a cone.",
+    long_description=open("README.md").read(),
+    description="Paket untuk menghitung berbagai bangun ruang.",
     author="Kelompok 4 ALGO A",
     author_email="office@unhas.ac.id",
+    url="https://github.com/nurulmarisa8/geometri-package.git",
     license="MIT",
 )
